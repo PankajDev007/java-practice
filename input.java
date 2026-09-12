b0171c27f11c328c404149d6c4397a2384c47b47
PankajDev007 <pankajadhe12@gmail.com>
2026-09-12 21:11:48 +0530

    Initial commit

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter name:");
+        String name=sc.next();
+        System.out.print("Welcome"+" "+name);
+    }
+}
