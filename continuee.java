7981fe70b013761b074884d136446a61ea27bf05
PankajDev007 <pankajadhe12@gmail.com>
2026-09-16 17:56:54 +0000

    Sep 16, 2026, 11:26 PM

--- continuee.java
+++ continuee.java
@@ -2,9 +2,9 @@ import java.util.*;
 class continuee{
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int no;
+    
         do {
-            no=sc.nextInt();
+           int no=sc.nextInt();
             if(no%10==0) {
                 continue;
             }
