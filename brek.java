7d157da25ec5cba26cf8f195bd413f246abe8d5f
PankajDev007 <pankajadhe12@gmail.com>
2026-09-16 17:47:45 +0000

    Sep 16, 2026, 11:17 PM

--- brek.java
+++ brek.java
@@ -1,5 +1,14 @@
+import java.util.*;
 class brek{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner (System.in);
+        int no;
+        do {
+            no=sc.nextInt();
+            if(no%10==0) {
+                break;
+            }
+            System.out.println(no);
+        }while(no!=0);
     }
 }
