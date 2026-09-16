f6bcd2c39a6369e455564909d0c9238b39773ff5
PankajDev007 <pankajadhe12@gmail.com>
2026-09-16 15:33:28 +0000

    Sep 16, 2026, 9:03 PM

--- rev.java
+++ rev.java
@@ -1,14 +1,11 @@
 class rev{
     public static void main(String[] args) {
         int no = 9473;
-        while(no<0){
+        while(no>0){
             int digit = no%10;
             System.out.print(digit);
             no = no/10;
-            no++;
-
         }
-        System.out.println();
 
     }
 }
