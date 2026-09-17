9fd1d0b72a33eff2b39eb5393f8c4a60994650d0
PankajDev007 <pankajadhe12@gmail.com>
2026-09-17 14:59:40 +0000

    Sep 17, 2026, 8:29 PM

--- star_pattern.java
+++ star_pattern.java
@@ -1,3 +1,11 @@
 class star_pattern {
-    
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.println("*");
+            }
+
+        }
+        System.out.println();
+    }
 }
