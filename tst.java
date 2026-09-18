638fbbfeb2463b599f4d9a19366d24f53b5ae88d
PankajDev007 <pankajadhe12@gmail.com>
2026-09-18 10:44:37 +0000

    Sep 18, 2026, 4:14 PM

--- tst.java
+++ tst.java
@@ -14,6 +14,19 @@ class tst {
             }
             System.out.println();
         }
+        for(int i=n;i<=1;i--) {
+            for(int j=1;j<=(n-1);j++) {
+                System.out.print(" ");
+                
+            }
+            for(int j=1;j<=(2*i)-1;j++) {
+                System.out.print("*");
+                
+
+                
+            }
+            System.out.println();
+        }
         
     }
 }
