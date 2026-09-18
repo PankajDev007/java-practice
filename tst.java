5313ea94627a06c515ca2166240fcf95ddb9572c
PankajDev007 <pankajadhe12@gmail.com>
2026-09-18 10:36:19 +0000

    Sep 18, 2026, 4:06 PM

--- tst.java
+++ tst.java
@@ -1,13 +1,17 @@
 class tst {
     public static void main(String[] args) {
-        int n=4;
+        int n=4,m=1;
         for(int i=1;i<=n;i++) {
             for(int j=1;j<=(n-1);j++) {
                 System.out.print(" ");
                 
             }
-            for(int j=1;j<=(n+2);j++) {
+            for(int j=1;j<=(m+2);j++) {
                 System.out.print("*");
+                
+            }
+            for(int j=1;j<=(n-1);j++) {
+                System.out.print(" ");
                 
             }
             System.out.println();
