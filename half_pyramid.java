edf5ed235e351e6e74e3710559c346ec1c4be442
PankajDev007 <pankajadhe12@gmail.com>
2026-09-18 09:31:37 +0000

    Sep 18, 2026, 3:01 PM

--- half_pyramid.java
+++ half_pyramid.java
@@ -1,11 +1,8 @@
 class half_pyramid {
     public static void main(String[] args) {
-        for(int i=1;i<=4;i++) {
-            for(int j=1;j<=i;j++) {
-                System.out.print("*");
-                
-            }
-            System.out.println();
+        int n=5;
+        for(int i=1;i<=n;i--) {
+            System.out.println("*");
         }
         
     }
