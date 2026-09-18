ce10f49e440ac594b53824c2e46955802d3e140d
PankajDev007 <pankajadhe12@gmail.com>
2026-09-18 16:07:23 +0000

    Sep 18, 2026, 9:37 PM

--- patterns.java
+++ patterns.java
@@ -41,6 +41,18 @@ class patterns {
                 }
                 
             }
+        public static void floyds_triangle(int n) {
+            int counter=1;
+            for(int i=1;i<=n;i++) {
+                for(int j=1;j<=(n-i);j++) {
+                    System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++){
+                    System.out.print(counter);
+                    counter++;                    
+                }
+            }
+        }
     
     public static void main(String[] args) {
         //hollow_rectangle(4,5);
