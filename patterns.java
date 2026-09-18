9025968e0f0835ce3b62560bd9e6e92d99dd855b
PankajDev007 <pankajadhe12@gmail.com>
2026-09-18 15:45:48 +0000

    Sep 18, 2026, 9:15 PM

--- patterns.java
+++ patterns.java
@@ -25,10 +25,24 @@ class patterns {
             }
             
         }
+
+        public static void inv_rot_half_pyra_with_no(int n){
+            for(int i=1;i<=n;i++) {
+                for(int j=1;j<=(n-i);j++) {
+                        System.out.print(" ");
+                    }
+                    for(int j=1;j<=i;j++) {
+                        System.out.print(i);
+                    }
+                    System.out.println();
+                }
+                
+            }
     
     public static void main(String[] args) {
         //hollow_rectangle(4,5);
-        inv_rot_half_pyra(4);
+        //inv_rot_half_pyra(4);
+        inv_rot_half_pyra_with_no(4);
         
     }
 }
