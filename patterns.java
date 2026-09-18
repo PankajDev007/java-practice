04e8cbc7aa45f4525c8a0e94b7feb2266b87ee76
PankajDev007 <pankajadhe12@gmail.com>
2026-09-18 15:27:05 +0000

    Sep 18, 2026, 8:57 PM

--- patterns.java
+++ patterns.java
@@ -2,7 +2,7 @@ class patterns {
     public static void hollow_rectangle(int rows,int cols) {
         for(int i=1;i<=rows;i++) {
             for(int j=1;j<=cols;j++) {
-                if(i==1||i==cols||j==1||j==rows) {
+                if(i==1||i==rows||j==1||j==cols) {
                     System.out.print("*");
                 }
                 else {
