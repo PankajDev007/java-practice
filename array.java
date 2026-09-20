86aca1923c53905ff7072b0d9a2b62f1767ec6af
PankajDev007 <pankajadhe12@gmail.com>
2026-09-20 16:02:27 +0000

    Sep 20, 2026, 9:32 PM

--- array.java
+++ array.java
@@ -1,22 +1,13 @@
 import java.util.*;
 class array{
-    public static Scanner sc=new Scanner(System.in);
-    public static int arr[]=new int[20];
-    public static void create(){
-        System.out.println("Array is created");
-    }
-    public static void insert(){
-        for(int i=0;i<=arr.length;i++){
-            arr[i]=sc.nextInt();
-        }
-    }
     public static void main(String[] args) {
-        create();
+        int arr[]=new int[20];
+        System.out.println("Array is created");
+        Scanner sc=new Scanner(System.in);
         System.out.println("How many elements you want to insert ?");
         int ch=sc.nextInt();
-        for(int j=0;j<ch;j++){
-            System.out.println("Enter element"+j);
-            insert();
+        for(int i=0;i<=ch;i++){
+            arr[i]=sc.nextInt();
         }
 
     }
