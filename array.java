073d2d9204c4fafc8178f86c0c699763abff6616
PankajDev007 <pankajadhe12@gmail.com>
2026-09-20 16:29:24 +0000

    Sep 20, 2026, 9:59 PM

--- array.java
+++ array.java
@@ -5,7 +5,7 @@ class array{
     public static void insert(){
         System.out.print("How many elements you want to insert ?");
         int ch=sc.nextInt();
-        for(int i=1;i<=ch;i++){
+        for(int i=0;i<ch;i++){
             System.out.print("Enter element"+i+":");
             arr[i]=sc.nextInt();
         }
