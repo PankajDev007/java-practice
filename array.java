b7de64afe2f65e3af3a3bfa2a892f657a51f353e
PankajDev007 <pankajadhe12@gmail.com>
2026-09-20 16:27:55 +0000

    Sep 20, 2026, 9:57 PM

--- array.java
+++ array.java
@@ -14,7 +14,7 @@ class array{
     }
     public static void display(){
         System.out.println("Array is:");
-        for(int i=1;i<=arr.length;i++){
+        for(int i=0;i<=arr.length;i++){
             System.out.println(arr[i]);
         }
     }
