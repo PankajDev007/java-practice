51519e0c4f6290f1a8ff025406627638741e0e33
PankajDev007 <pankajadhe12@gmail.com>
2026-09-20 15:50:53 +0000

    Sep 20, 2026, 9:20 PM

---
+++ array.java
@@ -0,0 +1,9 @@
+class array{
+    public static void create(){
+        int arr[]=new int[20];
+        System.out.println("Array is created");
+    }
+    public static void main(String[] args) {
+        create();
+    }
+}
