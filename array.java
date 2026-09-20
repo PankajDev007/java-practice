9c1d965de4de96c0638fe27a990e92df223442d2
PankajDev007 <pankajadhe12@gmail.com>
2026-09-20 16:24:53 +0000

    Sep 20, 2026, 9:54 PM

--- array.java
+++ array.java
@@ -12,11 +12,14 @@ class array{
         System.out.println("Inserted !!");
     
     }
-    public static void main(String[] args) {
-        insert();
+    public static void display(){
         System.out.println("Array is:");
         for(int i=1;i<=arr.length;i++){
             System.out.println(arr[i]);
         }
+    }
+    public static void main(String[] args) {
+        insert();
+        display();
     }
 }
