c560e342ed588e8ab0871a0390d444bd4246efeb
PankajDev007 <pankajadhe12@gmail.com>
2026-09-23 01:52:20 +0000

    Sep 23, 2026, 7:22 AM

---
+++ binary_search.java
@@ -0,0 +1,9 @@
+class binary_search{
+    public static void main(String[] args) {
+        int arr[]={23,42,12,18,93};
+        System.out.println("Unsorted Array:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
