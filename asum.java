74d5f87421090be42b3b41f1aac6ed8c750032ba
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 14:10:17 +0000

    Sep 24, 2026, 7:40 PM

--- asum.java
+++ asum.java
@@ -1,3 +1,10 @@
 class asum{
-    
+    public static void main(String[] args) {
+        int arr[]={32,43,67,43,89};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("Sum="+sum);
+    }
 }
