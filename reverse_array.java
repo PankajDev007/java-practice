d42f9bcba35c975b329b5edd0b3d0ab8cd70b43b
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 15:10:13 +0000

    Sep 24, 2026, 8:40 PM

--- reverse_array.java
+++ reverse_array.java
@@ -7,10 +7,10 @@ class reverse_array{
                 int temp=f;
                 f=l;
                 l=temp;
+                System.out.println(arr[i]);
             }
+            
         }
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+        
     }
 }
