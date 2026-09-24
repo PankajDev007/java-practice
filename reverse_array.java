68842c5c6800e7f90ce97532a75c746c112a790a
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 15:22:43 +0000

    Sep 24, 2026, 8:52 PM

--- reverse_array.java
+++ reverse_array.java
@@ -2,14 +2,15 @@ class reverse_array{
     public static void main(String[] args) {
         int arr[]={21,34,56,23,67};
         int f=0,l=arr.length-1;
-        while(f<l){
+        for(int i=0;i<arr.length;i++){
+        if(f<l){
                 int temp=arr[l];
                 arr[l]=arr[f];
                 arr[f]=temp;
             f++;
             l--; 
         }
-        for(int i=0;i<arr.length;i++){
+        
             System.out.println(arr[i]);
         }
         
