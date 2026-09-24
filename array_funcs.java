064a0346854ec3bbdee7ab06da4ef685940e06cd
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 16:47:44 +0000

    Sep 24, 2026, 10:17 PM

--- array_funcs.java
+++ array_funcs.java
@@ -14,7 +14,7 @@ class array_funcs{
     }
     public static void pairs(int arr[]){
         int current;
-        for(int i=arr[0];i<arr.length;i++){
+        for(int i=0;i<arr.length;i++){
             current=arr[i];
             for(int j=i+1;j<arr.length;j++){
                 System.out.print("("+current+","+arr[j]+")");
