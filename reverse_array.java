b55e2544d64e3b1859f66164fca76dc0db9df953
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 15:08:18 +0000

    Sep 24, 2026, 8:38 PM

---
+++ reverse_array.java
@@ -0,0 +1,14 @@
+class reverse_array{
+    public static void main(String[] args) {
+        int arr[]={21,34,56,23,67};
+        int f=0,l=arr.length-1;
+        for(int i=0;i<arr.length;i++){
+            if(f<l){
+                int temp=f;
+                f=l;
+                l=temp;
+            }
+        }
+        System.out.println(arr);
+    }
+}
