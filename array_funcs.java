f94150a81ad71425e5951a12d55d3ea959ce58ed
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 16:30:27 +0000

    Sep 24, 2026, 10:00 PM

---
+++ array_funcs.java
@@ -0,0 +1,31 @@
+class array_funcs{
+    public static void reverse(int arr[]){
+        int f=0,l=arr.length-1;
+        while(f<l){
+            int temp=arr[l];
+            arr[l]=arr[f];
+            arr[f]=temp;
+        f++;
+        l--; 
+    }
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+    }
+    public static void pairs(int arr[]){
+        int current;
+        for(int i=0;i<arr.length;i++){
+            current=arr[i];
+            for(int j=i+1;j<arr.length;j++){
+                System.out.println("("+current+","+arr[j]+")");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={21,34,56,23,67};
+        //reverse(arr);
+        pairs(arr);
+        
+         
+    }
+}
