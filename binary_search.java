82ca43fdda6dc9482d066891c05aa41c343c819c
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 14:47:27 +0000

    Sep 24, 2026, 8:17 PM

--- binary_search.java
+++ binary_search.java
@@ -1,27 +1,19 @@
 class binary_search{
     public static void main(String[] args) {
         int arr[]={23,42,52,78,93};
-        System.out.println("Unsorted Array:");
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+        int start=0,end=arr.length-1;
+        int mid,key=52;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(mid==key){
+                System.out.println("Found at "+mid);
+            }
+            else if(mid>key){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
         }
-        int mid,key=42;
-        int start,end=arr.length-1;
-        boolean found=false;
-        for(start=arr[0];start<end;start++){
-        mid=(start+end)/2;
-        if(mid==key){
-            found=true;
-        }
-        else if(mid<key){
-            start=mid+1;
-        }
-        else if(mid>key){
-            end=mid-1;
-        }
-    }
-        
-System.out.println(found);
-
     }
 }
