ff344a4bbf9b8487bc15f53191fc1ff6fd6c3a48
PankajDev007 <pankajadhe12@gmail.com>
2026-09-24 17:04:57 +0000

    Sep 24, 2026, 10:34 PM

--- array_funcs.java
+++ array_funcs.java
@@ -23,17 +23,17 @@ class array_funcs{
         }
     }
     public static void subarray(int arr[]){
-        int current,end;
         for(int i=0;i<arr.length;i++){
     
             for(int j=i+1;j<arr.length;j++){
             
             for(int k=i;k<j;k++){
-                System.out.print(arr[k]);
+                System.out.print(arr[k]+" ");
             }
         }
             System.out.println();
         }
+        System.out.println();
     }
     public static void main(String[] args) {
         int arr[]={21,34,56,23,67};
