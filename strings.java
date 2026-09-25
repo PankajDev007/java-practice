882968ff23d347768ba24f857020742b1f348ea9
PankajDev007 <pankajadhe12@gmail.com>
2026-09-25 22:48:59 +0000

    Sep 26, 2026, 4:18 AM

--- strings.java
+++ strings.java
@@ -1,7 +1,15 @@
+import java.util.*;
 class strings{
     public static void main(String[] args) {
         String str="abcd";
         String str2=new String("Pankaj");
         System.out.println(str2);
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first name:");
+        str=sc.nextLine();
+        System.out.print("Enter last name:");
+        str2=sc.nextLine();
+        String str3=str+" "+str2;
+        System.out.println("Full name="+str3);
     }
 }
