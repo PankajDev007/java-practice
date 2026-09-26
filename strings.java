1cf40b4bd4711b1a391d5922359e9db8b0b512f9
PankajDev007 <pankajadhe12@gmail.com>
2026-09-26 23:53:27 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class strings{
+    public static void func(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i));
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        String str1;
+        String str2;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first name:");
+        str1=sc.nextLine();
+        System.out.print("Enter last name:");
+        str2=sc.nextLine();
+        String str3=str1+" "+str2;
+        //System.out.println("Full name="+str3);
+        func(str1);
+    }
+}
