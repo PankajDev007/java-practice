b740d210c80b429b3141ff890078b2cab517e45c
PankajDev007 <pankajadhe12@gmail.com>
2026-09-26 10:03:39 +0000

    Sep 26, 2026, 3:33 PM

---
+++ Pallindrome.java
@@ -0,0 +1,15 @@
+class Pallindrome{
+public static void main(String[] args) {
+        String str="aba";
+        String str2="";
+        for(int i=str.length()-1;i>=0;i--){
+            str2=str2+str.charAt(i);
+        }
+        if(str==str2){
+            System.out.println("Pallindrome");
+        }
+        else{
+            System.out.println("Non Pallindrome");
+        }
+    }
+}
